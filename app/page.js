66969801974ff8bc47af0e36758445a9771eebfa
Elijah <iamngunjiri@gmail.com>
2025-08-28 11:12:07 +0300

    Footer component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
       Hey this is next
       <br/>
-      <Button>Button</Button>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }

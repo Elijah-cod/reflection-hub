01e7543dc4ce7ab8d2e486c12570dea924570dc7
Elijah <iamngunjiri@gmail.com>
2025-09-17 20:28:09 +0300

    Add journal entry page

--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -1,8 +1,143 @@
+"use client"
+
+import { journalSchema } from "@/app/lib/schema";
+import dynamic from "next/dynamic";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import 'react-quill-new/dist/quill.snow.css';
+import { BarLoader } from "react-spinners";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { getMoodById, MOODS } from "@/app/lib/moods";
+import { is } from "date-fns/locale";
+import { Button } from "@/components/ui/button";
+
+const ReactQuill = dynamic(()=> import("react-quill-new"), {ssr: false})
 
 const JournalEntryPage = () => {
+    const {register, handleSubmit, control, formState: {errors}, watch}= useForm({
+        resolver: zodResolver(journalSchema),
+        defaultValues: {
+            title: "",
+            content: "",
+            mood: "",
+            collectionId: "",
+        },
+    })
+
+    const isLoading = false
+
+    const onSubmit = handleSubmit(async() => {})
+
     return(
-        <div>Journal Entry Page</div>
+        <div className="py-8">
+            <form className="space-y-2 mx-auto" onSubmit={onSubmit}>
+                <h1 className="text-5xl md:text-6xl gradient-title">What&apos;s on your mind</h1>
+
+                {isLoading && <BarLoader color="orange" width={"100%"}/>}
+
+                <div className="space-y-2">
+                    <label className="text-sm font-medium">Title</label>
+                    <Input disabled = {isLoading} {...register("title")} placeholder="Give your entry a title..." className={`py-5 md-text-md ${errors.title? "border-red-500" : ""}`}/>
+                    {
+                        errors.title && (
+                            <p className="text-red-500 text-sm">{errors.title.message}</p>
+                        )
+                    }
+                </div>
+
+                <div className="space-y-4">
+                    <label className="text-sm font-medium">How are you feeling?</label>
+                    <Controller 
+                    name="mood"
+                    control={control}
+                        render={({field})=>(
+                            <Select onValueChange = {field.onChange} value={field.value}>
+                                <SelectTrigger className={errors.mood ? "border-red-500": ""}>
+                                    <SelectValue placeholder="Select a mood..." />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {
+                                        Object.values(MOODS).map((mood)=>(
+                                            <SelectItem key={mood.id} value={mood.id}>
+                                                <span>{mood.emoji} {mood.label}</span>
+                                            </SelectItem>
+                                        ))
+                                    }
+                                </SelectContent>
+                            </Select>
+                        )}
+                    />
+                    {
+                        errors.mood && (
+                            <p className="text-red-500 text-sm">{errors.mood.message}</p>
+                        )
+                    }
+                </div>
+
+
+
+                <div className="space-y-5">
+                    <label className="text-sm font-medium">{getMoodById(watch("mood"))?.prompt ?? "Write your thoughts..."}</label>
+                    <Controller 
+                    name="content"
+                    control={control}
+                        render={({field}) => (
+                            <ReactQuill 
+                                readOnly = {isLoading}
+                                theme="snow"
+                                value={field.value}
+                                onChange={field.onChange}
+                                modules={
+                                    {
+                                        toolbar: [
+                                            [{header: [1, 2, 3, false]}],
+                                            ["bold", "italic", "underline", "strike"],
+                                            [{list: "ordered"}, {list: "bullet"}],
+                                            ["blockquote", "code-block"],
+                                            ["link"],
+                                            ["clean"],
+                                        ],
+                                    }
+                                }
+                            />
+                        )}
+                    />
+                    {
+                        errors.content && (
+                            <p className="text-red-500 text-sm">{errors.content.message}</p>
+                        )
+                    }
+                </div>
+
+
+                <div className="space-y-2">
+                    <label className="text-sm font-medium">Add to Collection (optional)</label>
+                    {/* <Controller 
+                    name="content"
+                    control={control}
+                        render={({field}) => (
+                        )}
+                    /> */}
+                    {
+                        errors.collectionId && (
+                            <p className="text-red-500 text-sm">{errors.collectionId.message}</p>
+                        )
+                    }
+                </div>
+
+                <div className="space-y-4 flex">
+                    <Button type="submit" variant="journal">Publish</Button>
+                </div>
+            </form>
+        </div>
     )
 }
 
